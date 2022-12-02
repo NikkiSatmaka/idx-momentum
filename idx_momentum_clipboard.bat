0985b22bf51faabbf72e8fdab6d2f3d86338db27
Nikki Satmaka <nsatmaka@gmail.com>
2022-12-03 00:05:11 +0700

    refactor: change env to .env

--- idx_momentum_clipboard.bat
+++ idx_momentum_clipboard.bat
@@ -1,4 +1,5 @@
 @echo off
-@CALL conda.bat activate "C:\Users\nikki\workspace\algotrading\idxmomentumbot\env"
+@CALL conda.bat activate "C:\Users\nikki\workspace\algotrading\idxmomentumbot\.env"
 @python.exe "C:\Users\nikki\workspace\algotrading\idxmomentumbot\app.py"
 @pause
+
--- idx_momentum_clipboard.sh
+++ idx_momentum_clipboard.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source $HOME/mambaforge/etc/profile.d/conda.sh
-conda activate $HOME/workspace/algotrading/idxmomentumbot/env
+conda activate $HOME/workspace/algotrading/idxmomentumbot/.env
 python $HOME/workspace/algotrading/idxmomentumbot/app.py
 read
+
